25853ebc5dea961ff0bfe2e829eb81bf0683cc98
GHJ5368 <jesantbar@alum.us.es>
2024-09-30 12:04:41 +0200

    modified:   src/ahorcado.py 	modified:   src/ahorcado_test.py

--- src/ahorcado.py
+++ src/ahorcado.py
@@ -1,3 +1,5 @@
+import random
+
 def cargar_palabras(ruta):
     with open(ruta, encoding='utf-8') as f:
         res = []
@@ -6,3 +8,17 @@ def cargar_palabras(ruta):
             
         return res
 
+def elegir_palabra(lista):
+    palabra=random.choice(lista)
+    return palabra
+
+def enmascarar_palabra(palabra,letras_probadas):
+    palabra_enmascarada=[]
+    res=""
+    for letra in palabra:
+        if letra in letras_probadas:
+            palabra_enmascarada.append(letra)
+        else:
+            palabra_enmascarada.append("_")
+    res = ''.join(palabra_enmascarada)
+    return res
--- src/ahorcado_test.py
+++ src/ahorcado_test.py
@@ -42,10 +42,10 @@ def test_ejecutar_turno(palabra_secreta, letras_probadas):
     print()
 
 if __name__ == "__main__":
-    test_cargar_palabras("data/palabras_ahorcado.txt")
+    #test_cargar_palabras("data/palabras_ahorcado.txt")
     #PALABRAS = cargar_palabras("data/palabras_ahorcado.txt")
     #test_elegir_palabra(PALABRAS)
-    #test_enmascarar_palabra('python', {})
+    test_enmascarar_palabra('python', [])
     #test_enmascarar_palabra('python', {'p', 'y', 't', 'h', 'o', 'n'})
     #test_enmascarar_palabra('python', {'a', 'b', 'c', 'd', 'e'})
     #test_enmascarar_palabra('python', {'a', 'e', 'i', 'o', 'u'})
